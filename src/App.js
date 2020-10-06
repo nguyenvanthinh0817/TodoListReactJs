0efd4bfca3359b1c8499639744d34dec5798760d
Nguyen van thinh <nguyenvanthinh0817@gmail.com>
2020-10-06 16:05:45 +0700

    Hoan thien cac chuc nang co ban, chua co chuc nang them moi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
+import React, { useState } from 'react';
+import './App.scss';
+import TodoList from './components/TodoList';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = [
+        { id: '1', title: 'di choi', status: 'C' },
+        { id: '2', title: 'di sua xe', status: 'D' },
+        { id: '4', title: 'di hoc', status: 'X' },
+        { id: '5', title: 'di hoc', status: 'X' },
+        { id: '6', title: 'di hoc', status: 'X' },
+    ];
+    const [todoList, setTodoList] = useState(data);
+    const [timkiem, setTimkiem] = useState('');
+    function handleonClickXoa(x) {
+        const index = todoList.findIndex((todo) => x.id === todo.id);
+        if (index < 0) {
+            return;
+        }
+
+        const newTodoList = [...todoList];
+        newTodoList.splice(index, 1);
+        setTodoList(newTodoList);
+    }
+    function handleonClickBatDau(x) {
+        const index = todoList.findIndex((todo) => x.id === todo.id);
+        if (index < 0) {
+            return;
+        }
+
+        const newTodoList = [...todoList];
+        newTodoList.splice(index, 1);
+        x.status = 'D';
+        newTodoList.push(x);
+        setTodoList(newTodoList);
+    }
+    function handleonClickChuaLam(x) {
+        const index = todoList.findIndex((todo) => x.id === todo.id);
+        if (index < 0) {
+            return;
+        }
+
+        const newTodoList = [...todoList];
+        newTodoList.splice(index, 1);
+        x.status = 'C';
+        newTodoList.push(x);
+        setTodoList(newTodoList);
+    }
+    function handleonClickChuaXong(x) {
+        const index = todoList.findIndex((todo) => x.id === todo.id);
+        if (index < 0) {
+            return;
+        }
+
+        const newTodoList = [...todoList];
+        newTodoList.splice(index, 1);
+        x.status = 'D';
+        newTodoList.push(x);
+        setTodoList(newTodoList);
+    }
+    function handleonClickHoanThanh(x) {
+        const index = todoList.findIndex((todo) => x.id === todo.id);
+        if (index < 0) {
+            return;
+        }
+
+        const newTodoList = [...todoList];
+        newTodoList.splice(index, 1);
+        x.status = 'X';
+        newTodoList.push(x);
+        setTodoList(newTodoList);
+    }
+    function chua() {
+        return todoList.filter((todo) => todo.status === 'C');
+    }
+    function dang() {
+        return todoList.filter((todo) => todo.status === 'D');
+    }
+    function xong() {
+        return todoList.filter((todo) => todo.status === 'X');
+    }
+
+    function onChangeTimKiem(e) {
+        console.log(e.target.value);
+        setTimkiem(e.target.value);
+        //   handleTimKiem();
+    }
+
+    function handleTimKiem() {
+        console.log(timkiem);
+        const newTodoList = data.filter(
+            (todo) =>
+                todo.title
+                    .toLowerCase()
+                    .indexOf(timkiem.toLowerCase().trim()) >= 0,
+        );
+        console.log(newTodoList);
+        setTodoList(newTodoList);
+    }
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="header">
+                    <h2>Danh sách công việc</h2>
+                    <div className="timkiem">
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="timkiemtxt"
+                            onChange={onChangeTimKiem}
+                            value={timkiem}
+                            placeholder="Tìm kiếm"
+                        ></input>
+                        <button
+                            className="btn btn-outline-secondary"
+                            onClick={handleTimKiem}
+                        >
+                            Tìm
+                        </button>
+                    </div>
+                    <button className="btn btn-success">Thêm việc mới</button>
+                </div>
+                <TodoList
+                    chua={chua()}
+                    dang={dang()}
+                    xong={xong()}
+                    onClickXoa={handleonClickXoa}
+                    onClickBatDau={handleonClickBatDau}
+                    onClickChuaLam={handleonClickChuaLam}
+                    onClickChuaXong={handleonClickChuaXong}
+                    onClickHoanThanh={handleonClickHoanThanh}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,141 @@
+/** @format */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+    todos: PropTypes.array,
+    onTodoClick: PropTypes.func,
+};
+TodoList.defaultProps = {
+    todos: [],
+    onClickXoa: null,
+    onClickBatDau: null,
+    onClickChuaLam: null,
+    onClickChuaXong: null,
+};
+function TodoList(props) {
+    const {
+        chua,
+        dang,
+        xong,
+        onClickXoa,
+        onClickBatDau,
+        onClickChuaLam,
+        onClickChuaXong,
+        onClickHoanThanh,
+    } = props;
+
+    function handleonClickXoa(x) {
+        if (onClickXoa) {
+            onClickXoa(x);
+        }
+    }
+
+    function handleonClickBatDau(x) {
+        if (onClickBatDau) {
+            onClickBatDau(x);
+        }
+    }
+    function handleonClickChuaLam(x) {
+        if (onClickChuaLam) {
+            onClickChuaLam(x);
+        }
+    }
+
+    function handleonClickChuaXong(x) {
+        if (onClickChuaXong) {
+            onClickChuaXong(x);
+        }
+    }
+
+    function handleonClickHoanThanh(x) {
+        if (onClickHoanThanh) {
+            onClickHoanThanh(x);
+        }
+    }
+    return (
+        <div className="">
+            <div className="body row">
+                <div className="chua col-ms-12">
+                    <h3>Chưa làm</h3>
+                    {chua.map((x, index) => (
+                        <div className="card" key={index}>
+                            <div className="card-body">
+                                <h5 className="card-title">{x.title}</h5>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => handleonClickBatDau(x)}
+                                >
+                                    Bắt đầu làm
+                                </button>
+                                <button
+                                    className="btn btn-danger"
+                                    onClick={() => handleonClickXoa(x)}
+                                >
+                                    Xóa
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="dang col-ms-12">
+                    <h3>Đang làm</h3>
+                    {dang.map((x, index) => (
+                        <div className="card" key={index}>
+                            <div className="card-body">
+                                <h5 className="card-title">{x.title}</h5>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => handleonClickChuaLam(x)}
+                                >
+                                    Chưa làm
+                                </button>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => handleonClickHoanThanh(x)}
+                                >
+                                    Hoàn thành
+                                </button>
+                                <button
+                                    className="btn btn-danger"
+                                    onClick={() => handleonClickXoa(x)}
+                                >
+                                    Xóa
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="xong col-ms-12">
+                    <h3>Hoàn thành</h3>
+                    {xong.map((x, index) => (
+                        <div className="card" key={index}>
+                            <div className="card-body">
+                                <h5 className="card-title">{x.title}</h5>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => handleonClickChuaXong(x)}
+                                >
+                                    Chưa xong
+                                </button>
+                                <button
+                                    className="btn btn-danger"
+                                    onClick={() => handleonClickXoa(x)}
+                                >
+                                    Xóa
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="footer">
+                <h1>Hello may cung</h1>
+            </div>
+        </div>
+    );
+}
+
+export default TodoList;
